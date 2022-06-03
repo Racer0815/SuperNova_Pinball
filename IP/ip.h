fa7e5ccbd15ddfd6748d2413cce99ca4d6c4ed58
Racer_0815 <96726936+Racer0815@users.noreply.github.com>
2022-06-03 10:44:48 +0200

    Create ip.h

---
+++ IP/ip.h
@@ -0,0 +1,22 @@
+#ifndef IP_H
+#define IP_H
+#include <Arduino.h>
+
+extern int Sensor1;
+extern int Sensor2;
+extern int Sensor3;
+extern int Sensor4;
+extern int Sensor5;
+extern int Sensor6;
+
+extern int status_S1;
+extern int status_S2;
+extern int status_S3;
+extern int status_S4;
+extern int status_S5;
+extern int status_S6;
+
+void Scan_input();
+void Scan_input(Sensor);
+
+#endif
