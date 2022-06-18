280f68a0ad4d7d63c236290411f83bfafe82c023
Racer_0815 <96726936+Racer0815@users.noreply.github.com>
2022-06-18 18:10:26 +0200

    Added GetInput function

--- IP/InputProcessor.cpp
+++ IP/InputProcessor.cpp
@@ -1,4 +1,24 @@
 #include "InputProcessor.h"
 #include <Arduino.h> 
-void ScanSensors
+void ScanSensors{}
 
+
+
+void GetInput(Sensor)
+{
+  if(Sensor == 1){
+    return Status_S1;
+  }
+  if(Sensor == 2){
+    return Status_S2;
+  }
+  if(Sensor == 3)[
+    return Status_S3;
+  }
+  if(Sensor == 4){
+    return Status_S4;
+  }
+  if(Sensor == 5){
+    return Status_S5;
+  }
+}
