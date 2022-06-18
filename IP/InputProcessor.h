b8c6e410df59740592ab8cb148174464d6f9b5b3
Racer_0815 <96726936+Racer0815@users.noreply.github.com>
2022-06-18 18:03:30 +0200

    Update InputProcessor.h

--- IP/InputProcessor.h
+++ IP/InputProcessor.h
@@ -9,12 +9,12 @@ extern int Sensor4;
 extern int Sensor5;
 extern int Sensor6;
 
-extern int status_S1;
-extern int status_S2;
-extern int status_S3;
-extern int status_S4;
-extern int status_S5;
-extern int status_S6;
+int status_S1;
+int status_S2;
+int status_S3;
+int status_S4;
+int status_S5;
+int status_S6;
 
 void Scan_input();
 void Scan_input(Sensor);
